ccc118f6e26e66787f666f6f5ed58a5a841cab89
jmlco <jmlcoliveira@gmail.com>
2021-05-13 22:11:39 +0100

    added some methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,108 @@
-public class Main {
-    public static void main(String[] args) {
+import commands.Command;
+import database.*;
 
+import java.util.Scanner;
+
+/**
+ * Main class of the program which read commands from console and communicates with the database.
+ *
+ * @author Guilherme Pocas 60236, Joao Oliveira 61052
+ */
+public class Main {
+
+    /**
+     * Main method where Scanner and Database are initialized
+     *
+     * @param args contains the supplied command-line arguments as an array of String objects
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Database db = new DatabaseClass();
+        commands(in, db);
+        in.close();
+    }
+
+    /**
+     * Reads a command from console and executes the method associated with that command
+     *
+     * @param in input where the data will be read from
+     * @param db Database where this action will be performed
+     */
+    private static void commands(Scanner in, Database db) {
+        Command command = getCommand(in);
+        while (command != Command.EXIT) {
+            switch (command) {
+                case REGISTER:
+                    break;
+
+                case USERS:
+                    break;
+
+                case PROPERTY:
+                    break;
+
+                case PROPERTIES:
+                    break;
+
+                case BOOK:
+                    break;
+
+                case CONFIRM:
+                    break;
+
+                case REJECT:
+                    break;
+
+                case REJECTIONS:
+                    break;
+
+                case PAY:
+                    break;
+
+                case REVIEW:
+                    break;
+
+                case HOST:
+                    break;
+
+                case GUEST:
+                    break;
+
+                case STAYS:
+                    break;
+
+                case SEARCH:
+                    break;
+
+                case BEST:
+                    break;
+
+                case GLOBALTROTTER:
+                    break;
+
+                case HELP:
+                    break;
+
+                case EXIT:
+                    break;
+
+            }
+            command = getCommand(in);
+        }
+    }
+
+    /**
+     * Returns Command which corresponds to the command given by the user
+     *
+     * @param in input where the data will be read from
+     * @return Command which corresponds to the command given by the user
+     */
+    private static Command getCommand(Scanner in) {
+        try {
+            String comm = in.next().toUpperCase();
+            return Command.valueOf(comm);
+        } catch (Exception e) {
+            return Command.UNKNOWN;
+        }
     }
 }
